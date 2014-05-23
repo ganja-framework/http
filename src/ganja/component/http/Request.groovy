f9558fe1d2bf5aab59611097153754f5e50ba7ee
Janusz Slota <janusz.slota@nixilla.com>
2014-05-23 18:37:28 +0100

    Added specs for Request object

--- src/ganja/component/http/Request.groovy
+++ src/ganja/component/http/Request.groovy
@@ -1,4 +1,43 @@
 package ganja.component.http
 
 class Request {
+
+    String method
+    Integer port
+    String scheme
+    String host
+
+    Map<String, String> attributes = [:]
+    Map<String, Object> parameters = [:]
+    Map<String, String> headers = [:]
+
+    String getAttribute(String key) {
+
+        attributes[key]
+    }
+
+    void setAttribute(String key, String value) {
+
+        attributes.put(key, value)
+    }
+
+    def getParameter(String key) {
+
+        parameters[key]
+    }
+
+    void setParameter(String key, Object value) {
+
+        parameters[key] = value
+    }
+
+    String getHeader(String key) {
+
+        headers[key]
+    }
+
+    void setHeader(String key, String value) {
+
+        headers[key] = value
+    }
 }
--- test/ganja/component/http/RequestSpec.groovy
+++ test/ganja/component/http/RequestSpec.groovy
@@ -13,4 +13,60 @@ class RequestSpec extends Specification {
         request instanceof Request
     }
 
+    void "it can have attributes"() {
+
+        given:
+        def request = new Request()
+
+        when:
+        request.setAttribute('test','value')
+
+        then:
+        request.getAttribute('test') == 'value'
+    }
+
+    void "it can have parameters"() {
+
+        given:
+        def request = new Request()
+        def value = [:]
+
+        when:
+        request.setParameter('test','value')
+        request.setParameter('complex', value)
+
+        then:
+        request.getParameter('test') == 'value'
+        request.getParameter('complex') == value
+    }
+
+    void "it can have headers"() {
+
+        given:
+        def request = new Request()
+
+        when:
+        request.setHeader('Content-Type','application/json')
+
+        then:
+        request.getHeader('Content-Type') == 'application/json'
+    }
+
+    void "it knows its method, port, scheme and host"() {
+
+        given:
+        def request = new Request()
+
+        when:
+        request.setMethod('POST')
+        request.setPort(80)
+        request.setScheme('http')
+        request.setHost('localhost')
+
+        then:
+        request.getMethod() == 'POST'
+        request.getPort() == 80
+        request.getScheme() == 'http'
+        request.getHost() == 'localhost'
+    }
 }
