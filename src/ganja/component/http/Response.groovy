a9ab89c8297a18eae151ccbcecb1283f97d75f7c
Janusz Slota <janusz.slota@nixilla.com>
2014-09-19 17:16:15 +0100

    Set default content type and encoding

--- src/ganja/component/http/Response.groovy
+++ src/ganja/component/http/Response.groovy
@@ -6,6 +6,6 @@ class Response implements ServletResponseWriterTrait {
 
     String content
     Integer statusCode = 200
-    String contentType
+    String contentType = 'text/html; charset=UTF-8'
 
 }
