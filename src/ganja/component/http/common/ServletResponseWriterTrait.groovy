c97b0a9d604f8e092923d730085ec83fe1c3f702
Janusz Slota <janusz.slota@nixilla.com>
2014-09-20 12:22:54 +0100

    UTF8 test

--- src/ganja/component/http/common/ServletResponseWriterTrait.groovy
+++ src/ganja/component/http/common/ServletResponseWriterTrait.groovy
@@ -4,9 +4,9 @@ trait ServletResponseWriterTrait {
 
     void writeTo(def servletResponse) {
 
-        servletResponse.getWriter().write(content)
         servletResponse.setContentType(contentType)
         servletResponse.setStatus(statusCode)
         servletResponse.setCharacterEncoding(encoding)
+        servletResponse.getWriter().write(content)
     }
 }
