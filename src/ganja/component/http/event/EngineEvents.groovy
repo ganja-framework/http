e272e4c5a4ba805e60e478abbf97b156d1a3628d
Janusz Slota <janusz.slota@nixilla.com>
2015-04-12 17:59:26 +0100

    Changed controller event name

--- src/ganja/component/http/event/EngineEvents.groovy
+++ src/ganja/component/http/event/EngineEvents.groovy
@@ -3,6 +3,6 @@ package ganja.component.http.event
 class EngineEvents {
     static final String REQUEST = 'event.request'
     static final String RESPONSE = 'event.response'
-    static final String CONTROLLER = 'event.listener'
+    static final String CONTROLLER = 'event.controller'
     static final String EXCEPTION = 'event.exception'
 }
